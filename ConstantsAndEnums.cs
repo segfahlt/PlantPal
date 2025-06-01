acf535d1fe4336d239f9f2d2117cf5eb066717bf
Bill Long <segfahlt@gmail.com>
2025-05-31 20:53:08 -0400

    add PAT for pushing to PlantPal-Data Repo

--- ConstantsAndEnums.cs
+++ ConstantsAndEnums.cs
@@ -6,4 +6,6 @@ public class Constants
 	public readonly static string DataPath = Path.Combine(RepoPath, "data");
 	public readonly static string ImagePath = Path.Combine(RepoPath, "images");
 	public const string DataRepoUrl = "https://github.com/segfahlt/PlantPal-Data.git";
+	public const string PlantPalDataPatEnvVariable = "PlantPal-Data-PAT";
+
 }
